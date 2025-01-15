78a09cf418d5aca135f3a87f0d8615b0f22c3802
Pedro Henrique Marques de Souza <marquesdesouzap214@gmail.com>
2025-01-15 16:32:07 -0300

    primeiro JS

---
+++ js/conveniosScroll.js
@@ -0,0 +1,7 @@
+const conveniosRow1 = document.getElementById("convenios-row-1")
+const conveniosRow2 = document.getElementById("convenios-row-2")
+
+let percentual = 15;
+
+conveniosRow1.style.transform = `translate(${percentual}%)`
+conveniosRow2.style.transform = `translate(-${percentual}%)`
