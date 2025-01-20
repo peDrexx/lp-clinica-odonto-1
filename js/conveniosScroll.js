26e6e1989b13c78fe5f1e7687dd5c452be0b5876
Pedro Henrique Marques de Souza <marquesdesouzap214@gmail.com>
2025-01-20 16:31:49 -0300

    CONSEGUI FAZER A ANIMAÇÃO DOS CONVÊNIOS!!!

--- js/conveniosScroll.js
+++ js/conveniosScroll.js
@@ -1,7 +1,24 @@
 const conveniosRow1 = document.getElementById("convenios-row-1")
 const conveniosRow2 = document.getElementById("convenios-row-2")
 
-let percentual = 15;
+// let percentual = 15;
 
-conveniosRow1.style.transform = `translate(${percentual}%)`
-conveniosRow2.style.transform = `translate(-${percentual}%)`
+// conveniosRow1.style.transform = `translate(${percentual}%)`
+// conveniosRow2.style.transform = `translate(-${percentual}%)`
+
+const conveniosRows = document.querySelectorAll('.convenios-row');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting) {
+            
+            console.log("Intersectando")
+        }
+        else {
+            console.log("Não intersectando")
+        }
+    })
+})
+
+conveniosRows.forEach((el) => observer.observe(el));
